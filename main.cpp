4d42e267b2082a84dec2e703a534e66fdb2eede8
Ariel Gonzalez <ariegonzalez@frba.utn.edu.ar>
2023-05-27 20:19:09 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
+int main (){
+    long double num = 1;
+    long double denom = 1;
+    long double sumas = 0;
+    long double pi = 0;
+    long double ciclos = 0;
+    
+     while (ciclos <= 1500001) {
+        sumas = num/denom;
+        pi+=sumas;
+        denom+=2;
+        num*=-1;
+        ciclos++;
+        }
+        cout << setprecision (7) << pi*4 << endl;
+}
